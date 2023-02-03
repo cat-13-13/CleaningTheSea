06f58bca02b6d7581e6e504cf3bab3ac8545cfce
Catalina Fernandez <catalinaf97@gmail.com>
2023-02-03 10:25:01 +0100

    complete 2.0

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 document.querySelector('.start-btn').onclick = event => {
     game.level = 0
+    document.querySelector('.level').innerHTML = game.level
     game.enemies = []
     game.friends = []
     game.killedEnemies = 0
+    document.querySelector('.deathsCounter').innerHTML = game.killedEnemies
     game.friendsLives = 3
     game.framesIndex = 0
     game.backgroundTrack.currentTime = 0
