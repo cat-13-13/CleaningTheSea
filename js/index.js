be1b7c520920cb255e96fff114087f0dcdcdce70
Catalina Fernandez <catalinaf97@gmail.com>
2023-02-03 09:32:05 +0100

    restart

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 document.querySelector('.start-btn').onclick = event => {
+    game.level = 0
+    game.enemies = []
+    game.friends = []
+    game.killedEnemies = 0
+    game.friendsLives = 3
+    game.framesIndex = 0
+    game.backgroundTrack.currentTime = 0
+    game.bubblesAudio.currentTime = 0
     game.init()
 }
